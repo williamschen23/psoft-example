c8141098ad0b888d3ce16d3f086883603b25dc8e
williamschen23 <chenwill2005@gmail.com>
2025-06-04 22:59:13 -0400

    user2

--- test.java
+++ test.java
@@ -1,7 +1,7 @@
 public class test {
     public static int sum(int[] arr){
         int total = 0;
-        for(int i = 0; i < arr.length+1; i++){
+        for(int i = 0; i < arr.length; i++){
             total += arr[i];
         }
         return total;
